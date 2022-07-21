be4d5c83f68e508ca70aa2c8c6f39910f7067f49
vijayakumarreddymutra <vijaykumar.99reddy@gmail.com>
2022-07-21 22:33:16 +0530

    final commit

--- secondscript.sh
+++ secondscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 for i in {1..20}; do
-    if [ $expr ($i % 2) -eq 0 ]; then
+    if [ $(expr $i % 2) -eq 0 ]; then
         echo "this is even numb $i"
     else
         echo "this is odd numb $i"
